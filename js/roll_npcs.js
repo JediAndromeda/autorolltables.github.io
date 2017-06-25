5a476d0f8c2170889085f49dc0be1b7b86e06305
JediAndromeda <catwasheretoo@gmail.com>
2017-06-25 15:59:45 +1000

    Update roll_npcs.js

--- js/roll_npcs.js
+++ js/roll_npcs.js
@@ -6,28 +6,28 @@ top.npcs = [
 
 
 
-{"title": "d6 The treasure hunter is...", "id":"d6thetreasurehunteris_treasurehunter", "roll":[
-"A veteran dungeon delver.",
+{"title": "The bounty hunter is...", "id":"d6thetreasurehunteris_treasurehunter", "roll":[
+"A veteran of war.",
 "A desperate thief.",
 "A reckless fortune seeker.",
 "A well-known adventurer.",
 "A brash, young fool.",
 "A student of history.",   ]},
-{"title": "d6 The treasure hunter has...", "id":"d6thetreasurehunterhas_treasurehunter", "roll":[
+{"title": "d6 The bounty hunter has...", "id":"d6thetreasurehunterhas_treasurehunter", "roll":[
 "A long, hooked nose.",
 "A cute little button nose.",
 "A scar below the left eye.",
 "Pockmarked cheeks.",
 "High cheek bones.",
 "A pointy chin.",   ]},
-{"title": "d6 The treasure hunter...", "id":"d6thetreasurehunter_treasurehunter", "roll":[
+{"title": "d6 The bounty hunter...", "id":"d6thetreasurehunter_treasurehunter", "roll":[
 "Hums or sings while walking.",
 "Constantly brushes the hair back from out of his or her face.",
 "Laughs maniacally at everything.",
 "Never smiles and never laughs.",
 "Scratches the elbow frequently.",
 "Picks at his or her teeth after meals.",   ]},
-{"title": "d12 The treasure hunter wears...", "id":"d12thetreasurehunterwears_treasurehunter", "roll":[
+{"title": "d12 The bounty hunter wears...", "id":"d12thetreasurehunterwears_treasurehunter", "roll":[
 "A nose ring.",
 "A dark red cape.",
 "Shiny leather boots.",
@@ -37,19 +37,19 @@ top.npcs = [
 "A tattoo on the forearm (d6): 1. dagger; 2. dragon; 3. nude figure; 4. skull; 5. snake; 6. thorns.",
 "A long dark ponytail.",
 "Luscious golden curls.",
-"A handkerchief in his breast pocket.",
+"An odd techno gauntlet.",
 "An open shirt and a very hairy chest.",
 "Extravagant mustaches.",   ]},
-{"title": "d8 The treasure hunter carries...", "id":"d8thetreasurehuntercarries_treasurehunter", "roll":[
+{"title": "d8 The bounty hunter carries...", "id":"d8thetreasurehuntercarries_treasurehunter", "roll":[
 "Some hemp rope and a silk handkerchief.",
 "Several over-sized sacks.",
 "Several torches and a much-used knife.",
-"A pocketbook of notes and maps.",
+"A datapad of notes and maps.",
 "A dagger in each boot.",
-"A beautiful antique scabbard.",
-"A well-worn lantern.",
+"A beautiful antique knife.",
+"A well-worn credit chip.",
 "Climbing gear—pitons, hammer, and rope.",   ]},
-{"title": "d6 The treasure hunter is looking for...", "id":"d6thetreasurehunterislookingfor_treasurehunter", "roll":[
+{"title": "d6 The bounty hunter is looking for...", "id":"d6thetreasurehunterislookingfor_treasurehunter", "roll":[
 "Rumors of treasure hoards.",
 "The location of an ancient crypt or ruin.",
 "A pair of strong arms to haul some gold.",
